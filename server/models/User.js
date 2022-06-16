95d9d878b0cad30e57ee09b53837d1e7d065c86f
Optical23 <aaron@thefergusons.com>
2022-06-15 20:00:46 -0500

    added user and authetication prep

--- server/models/User.js
+++ server/models/User.js
@@ -1,11 +1,60 @@
-const {default: mongoose } = require('mongoose');
+const { default: mongoose } = require('mongoose');
 const { Schema, model } = require('mongoose');
+const bcrypt = require('bcrypt');
+
+const opts = {toJson: {virtuals: true}};
 
 const userSchema = new Schema(
     {
+        full_name: {
+            type: String,
+            required: true,
+        },
+        email: {
+            type: String,
+            required: true,
+            unique: true,
+            match: [/.+@.+\..+/, 'NOT A VALID EMAIL ADDRESS']
+        },
+        password: {
+            type: String,
+            required: true, 
+            minlength: 6
+        },
+        event: [
+            {
+            type: Schema.Types.ObjectId,
+            ref: 'Event'
+            }
+        ],
+        band: [
+            {
+                type: Schema.Types.ObjectId,
+                ref: 'Band'
+            }
+        ],
 
-    }
+    }, opts
 );
+
+// set up pre-save middleware to create password
+userSchema.pre('save', async function(next) {
+    if (this.isNew || this.isModified('password')) {
+      const saltRounds = 10;
+      this.password = await bcrypt.hash(this.password, saltRounds);
+    }
+  
+    next();
+});
+
+// compare the incoming password with the hashed password
+userSchema.methods.isCorrectPassword = async function(password) {
+    return bcrypt.compare(password, this.password);
+};
+
+userSchema.virtual('event_length').get(function() {
+    return this.event.length;
+});
 
 const User = model('User', userSchema);
 module.exports = User;
